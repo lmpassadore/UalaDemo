9da0d571f709462bf8602a60fa76510a4ef3a9a2
lmpassadore <lmpassadore@gmail.com>
2019-01-21 18:56:45 -0300

    Configuración inicial de la lista y pedido de libros Se agrega permiso de internet

--- app/src/main/java/com/uala/demo/activity/LibraryActivity.java
+++ app/src/main/java/com/uala/demo/activity/LibraryActivity.java
@@ -2,14 +2,68 @@ package com.uala.demo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
+import com.uala.demo.App;
 import com.uala.demo.R;
+import com.uala.demo.adapter.BookAdapter;
+import com.uala.demo.model.Book;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LibraryActivity extends AppCompatActivity {
+
+    private BookAdapter adapter;
+    private ArrayList<Book> books = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
+
+        setUpRecyclerView();
+        requestBooks();
     }
+
+    private void setUpRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.library_recyclerview_books);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapter = new BookAdapter(books);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void requestBooks() {
+
+        App.awsClient.getBooks().enqueue(new Callback<ArrayList<Book>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Book>> call, Response<ArrayList<Book>> response) {
+
+                ArrayList<Book> obtainedBooks = response.body();
+                if (!response.isSuccessful() || obtainedBooks == null) {
+                    Toast.makeText(LibraryActivity.this, getString(R.string.library_service_error), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                books.addAll(obtainedBooks);
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Book>> call, Throwable t) {
+                Toast.makeText(LibraryActivity.this, getString(R.string.library_service_error), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
 }
--- app/src/main/java/com/uala/demo/adapter/BookAdapter.java
+++ app/src/main/java/com/uala/demo/adapter/BookAdapter.java
@@ -1,7 +1,52 @@
 package com.uala.demo.adapter;
 
-public class BookAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.uala.demo.R;
+import com.uala.demo.model.Book;
 
+import java.util.ArrayList;
+
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder> {
+
+    private ArrayList<Book> books;
+
+    class BookViewHolder extends RecyclerView.ViewHolder {
+
+        BookViewHolder(@NonNull final View itemView) {
+            super(itemView);
+
+        }
+
+    }
+
+    public BookAdapter(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+    @NonNull
+    @Override
+    public BookViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View bookView = LayoutInflater
+                .from(viewGroup.getContext())
+                .inflate(R.layout.item_library_book, viewGroup, false);
+
+        return new BookViewHolder(bookView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BookViewHolder bookViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return books != null ? books.size() : 0;
+    }
 
 }
